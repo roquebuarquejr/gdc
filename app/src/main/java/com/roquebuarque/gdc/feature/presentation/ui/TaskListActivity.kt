ef008896cceee2293409622444cd21b04ab34067
roque.buarque <roque.buarque@share-now.com>
2020-10-25 09:46:46 +0100

    task crud

---
+++ app/src/main/java/com/roquebuarque/gdc/feature/presentation/ui/TaskListActivity.kt
@@ -0,0 +1,11 @@
+package com.roquebuarque.gdc.feature.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TaskListActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
